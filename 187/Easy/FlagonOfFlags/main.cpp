4a29b53217272c5b1667d246b45ea0d4ed746674
TimberDragon <Woodenphoenix@gmail.com>
2014-11-06 01:49:11 -0500

    First pass at 187 easy code.

--- 187/Easy/FlagonOfFlags/main.cpp
+++ 187/Easy/FlagonOfFlags/main.cpp
@@ -6,6 +6,9 @@
  */
 
 #include <cstdlib>
+#include <map>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -13,7 +16,38 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    
+    string commandLine;
+    int numberOfInputs;
+    std::map<string,string> shortFlags;
+    std::map<string,string> longFlags;
+    
+    cin>>numberOfInputs;
+    if(numberOfInputs != NULL)
+    {
+        for(int i = 0; i < numberOfInputs; i++)
+        {
+            string input = "";
+            cin >> input;
+            if(input.compare("") != 0)
+            {
+                int indexOfSplit;
+                string shortFlag;
+                string longFlag;
+                
+                indexOfSplit = input.find_first_of(":");
+                
+                shortFlag = input.substr(0,indexOfSplit-1);
+                longFlag = input.substr(indexOfSplit+1);
+                
+                shortFlags[shortFlag] = longFlag;
+                longFlags[longFlag] = shortFlag;
+            }
+        }
+    }
+    
+    cin>>commandLine;
+    
     return 0;
 }
 
